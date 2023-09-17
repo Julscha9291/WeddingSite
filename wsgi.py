8399f648422510e7244b74ac299cb9f5810684b1
Julscha9291 <j.schaepermeier91@gmail.com>
2023-09-17 10:43:44 +0200

    Commit-Beschreibung

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,9 @@
 import os
 import django
 from django.core.wsgi import get_wsgi_application
+import sys
+
+sys.path.append('/home/pi/WeddingSite') 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WeddingSite.settings')
 
